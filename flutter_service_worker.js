c60ae0f91bf980bd29f3a7a9658800163cb27e8c
ThomasFercher <thomas.fercher@outlook.com>
2021-11-28 11:27:12 +0000

    Update from https://github.com/ThomasFercher/Legend-Design/commit/185b61da626c00239ed1c79b0a5f11e4ab57f581

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "ab231ee8dbeeb63b747c8635691a3c33",
-"/": "ab231ee8dbeeb63b747c8635691a3c33",
+  "index.html": "1bdb1c7e050ee26dd57b343cab5f78ca",
+"/": "1bdb1c7e050ee26dd57b343cab5f78ca",
 "main.dart.js": "6c83c8f7a3eeed53d1e097ac0beb9b31",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
